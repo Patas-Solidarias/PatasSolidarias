e5aff6a7ffbf2264d52ca7290211888b83bfbec2
guilherme.thomy <guilherme.thomy@allog.com.br>
2025-04-02 20:56:59 -0300

    feat: add application context

--- PatasSolidarias.Infra/Contexts/ApplicationDbContext.cs
+++ PatasSolidarias.Infra/Contexts/ApplicationDbContext.cs
@@ -4,6 +4,10 @@ namespace PatasSolidaras.Infra.Contexts;
 
 public class ApplicationDbContext : DbContext
 {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+    
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
