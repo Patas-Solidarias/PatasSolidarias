f32a337f0c4840db2131b36036d12507ea42e3f1
lonner007 <carlostsantos104@gmail.com>
2025-06-04 00:12:25 -0300

    Adiciona perfil empresa

--- PatasSolidarias.Frontend/src/app/pages/usuario/usuario.component.ts
+++ PatasSolidarias.Frontend/src/app/pages/usuario/usuario.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { PerfilComponent } from '../../components/perfil/perfil.component';
+import { PerfilEmpresaComponent } from '../../components/perfil/perfil-empresa/perfil-empresa.component';
 import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-usuario',
-  imports: [PerfilComponent, CommonModule],
+  imports: [PerfilComponent, PerfilEmpresaComponent, CommonModule],
   templateUrl: './usuario.component.html',
   styleUrls: ['./usuario.component.scss']
 })
 export class UsuarioComponent {
-  logado: boolean = false; // Altere para 'true' para simular um usuário logado
+  logado: boolean = true; // Altere para 'true' para simular um usuário logado
 
   constructor(private router: Router) {
     if (!this.logado) {
