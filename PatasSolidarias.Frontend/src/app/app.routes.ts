1d139f85b2898c7d54e8356019da85fcc18e4cec
Isadora M <91219205+PandaNico2@users.noreply.github.com>
2025-06-11 20:49:29 -0300

    Pagina de cadastro de postagem

--- PatasSolidarias.Frontend/src/app/app.routes.ts
+++ PatasSolidarias.Frontend/src/app/app.routes.ts
@@ -8,11 +8,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { VideoComponent } from './pages/video/video.component';
 import { UsuarioComponent } from './pages/usuario/usuario.component';
 import { LoginComponent } from './pages/login/login.component'; // login aqui
+import { SidebarAddComponent } from './components/sidebar-add/sidebar-add.component';
 
 export const Rotas = {
   PessoaCrudComponent: 'pessoa-crud',
   Home: 'home',
   Video: 'video',
+  Add: 'add',
   Usuario: 'usuario',
   Login: 'login'
 };
@@ -24,6 +26,7 @@ export const routes: Routes = [
     children: [
       { path: Rotas.Home, component: HomeComponent },
       { path: Rotas.Video, component: VideoComponent },
+      { path: Rotas.Add, component: SidebarAddComponent },
       { path: Rotas.Usuario, component: UsuarioComponent },
       { path: Rotas.PessoaCrudComponent, component: PessoaCrudComponent },
       { path: '', redirectTo: Rotas.Home, pathMatch: 'full' },
