89cf077e3a38125f0248633f4060ed236b724722
lonner007 <carlostsantos104@gmail.com>
2025-06-17 19:54:50 -0300

    Adiciona campanha

---
+++ PatasSolidarias.Api/Controllers/CampanhaController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
+using PatasSolidarias.Domain.Entities;
+using PatasSolidarias.Domain.Interfaces.Services;
+
+namespace PatasSolidarias.Backend.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CampanhaController: ControllersBase
+{
+    
+}
---
+++ PatasSolidarias.Domain/Entities/Campanha/Campanha.cs
@@ -0,0 +1,35 @@
+namespace PatasSolidarias.Domain.Entities.Campanha;
+
+public sealed class Campanha{
+    public int ID { get; private set; }
+    public string Titulo { get; private set; }
+    public string Descricao { get; private set;}
+    public DateTime DataInicio { get; private set; }
+    public DateTime DataFim { get; private set; }
+    public DateTime DataProgressao { get; private set; } 
+    public decimal MetaArrecadacao { get; private set; }
+    public decimal Progresso { get; private set;}
+    public int IdOng { get; private set;}
+
+
+    private Campanha() { }
+
+    public Campanha( string titulo, 
+                     string descricao,
+                     DateTime dataInicio,
+                     DateTime dataFim,
+                     DateTime dataProgressao,
+                     decimal metaArrecadacao,
+                     decimal progresso,
+                     int idOng) 
+    {
+        titulo = Titulo;
+        descricao = Descricao;
+        dataInicio = DataInicio;
+        dataFim = DataFim;
+        dataProgressao = DataProgressao;
+        metaArrecadacao = MetaArrecadacao;
+        progresso = Progresso;
+        idOng = IdOng;
+    }
+}
